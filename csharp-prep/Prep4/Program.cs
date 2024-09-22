eb6812c7eb848859da976f1458c0be4e41801c46
lop99003 <lop99003@byui.edu>
2024-09-21 20:54:59 -0500

    Learning Activity C# Prep 4 Complete and debugged.

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,49 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
-    }
+        List<int> numbers = new List<int>();
+        
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        int number = -1;
+        while (number != 0)
+        {
+            Console.Write("Enter number; ");
+
+            number = int.Parse(Console.ReadLine());
+
+            if (number != 0)
+            {
+                numbers.Add(number);
+            }
+        }
+        
+        int sum = 0;
+        foreach (int num in numbers)
+        {
+            sum += num;
+        }
+
+        Console.WriteLine($"The sum is: {sum}");
+
+        double average = 0;
+        if (numbers.Count > 0)
+        {
+            average = (double)sum / numbers.Count;
+        }
+
+        Console.WriteLine($"The average is: {average}");
+
+        int max = int.MinValue;
+        foreach (int num in numbers)
+        {
+            if (num > max)
+            {
+                max = num;
+            }
+        }
+        
+        Console.WriteLine($"The largest number is: {max}");
+    }    
+
 }
